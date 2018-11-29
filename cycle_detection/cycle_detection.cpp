fabcabe2e5efe44fd876f6786e192b568b5ad95f
chb19 <chikh.bdn@gmail.com>
2018-11-29 12:56:23 +0200

    output cycle

--- cycle_detection/cycle_detection.cpp
+++ cycle_detection/cycle_detection.cpp
@@ -1,19 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> g;
+vector<vector<int> > g;
 vector<char> color;
+vector<int> p;
 bool cycle_found;
 
 void dfs(int u, int parent)
 {
     color[u] = 1;
+    p[u] = parent;
     for (auto v : g[u])
     {
         if (color[v] == 1 and parent != v)
         {
-            cycle_found = true;
-            return;
+            int w = u;
+            while(w != v)
+            {
+                cout << w + 1 << " ";
+                w = p[w];
+            }
+            cout << v + 1 << endl;
+            exit(0);
         }
         if (color[v] == 0)
             dfs(v, u);
@@ -27,6 +35,7 @@ int main()
     cin >> n >> m;
     g.resize(n);
     color.resize(n);
+    p.resize(n);
     for (int i = 0; i < m; i++)
     {
         int u, v;
